f90168f09ccc2d7be20f3ecf881d7257a3c985ad
chalesyu <574249312@qq.com>
2022-06-06 08:30:52 +0800

    show md5 checksum for downloaded files

--- step1_dl_file.sh
+++ step1_dl_file.sh
@@ -8,6 +8,12 @@
 #wget https://downloads.openwrt.org/releases/22.03.0-rc1/targets/ramips/rt305x/openwrt-sdk-22.03.0-rc1-ramips-rt305x_gcc-11.2.0_musl.Linux-x86_64.tar.xz
 #wget https://cdn.kernel.org/pub/linux/kernel/projects/backports/stable/v5.15.33/backports-5.15.33-1.tar.xz
 
+md5sum v22.03.0-rc1.tar.gz
+md5sum openwrt-v22.03.0-rc1.tar.gz
+
+md5sum openwrt-sdk-22.03.0-rc1-ramips-rt305x_gcc-11.2.0_musl.Linux-x86_64.tar.xz
+md5sum backports-5.15.33-1.tar.xz
+
 echo
 echo Download build needed files complete.
 echo
