97694afcb2bc4df65304d3ed182634d71d63a5e3
chalesyu <574249312@qq.com>
2022-06-16 19:29:52 +0800

    bump version to v22.03.0-rc4

--- step1_dl_file.sh
+++ step1_dl_file.sh
@@ -2,16 +2,16 @@
 
 #Step1: Download files
 
-#wget https://github.com/openwrt/openwrt/archive/refs/tags/v22.03.0-rc1.tar.gz
-#wget https://gitlab.com/openwrt/openwrt/openwrt/-/archive/v22.03.0-rc1/openwrt-v22.03.0-rc1.tar.gz
+#wget https://github.com/openwrt/openwrt/archive/refs/tags/v22.03.0-rc4.tar.gz
+#wget https://gitlab.com/openwrt/openwrt/openwrt/-/archive/v22.03.0-rc4/openwrt-v22.03.0-rc4.tar.gz
 
-#wget https://downloads.openwrt.org/releases/22.03.0-rc1/targets/ramips/rt305x/openwrt-sdk-22.03.0-rc1-ramips-rt305x_gcc-11.2.0_musl.Linux-x86_64.tar.xz
+#wget https://downloads.openwrt.org/releases/22.03.0-rc4/targets/ramips/rt305x/openwrt-sdk-22.03.0-rc4-ramips-rt305x_gcc-11.2.0_musl.Linux-x86_64.tar.xz
 #wget https://cdn.kernel.org/pub/linux/kernel/projects/backports/stable/v5.15.33/backports-5.15.33-1.tar.xz
 
-md5sum v22.03.0-rc1.tar.gz
-md5sum openwrt-v22.03.0-rc1.tar.gz
+md5sum v22.03.0-rc4.tar.gz
+md5sum openwrt-v22.03.0-rc4.tar.gz
 
-md5sum openwrt-sdk-22.03.0-rc1-ramips-rt305x_gcc-11.2.0_musl.Linux-x86_64.tar.xz
+md5sum openwrt-sdk-22.03.0-rc4-ramips-rt305x_gcc-11.2.0_musl.Linux-x86_64.tar.xz
 md5sum backports-5.15.33-1.tar.xz
 
 echo
--- step4_start_make_drivers.sh
+++ step4_start_make_drivers.sh
@@ -2,10 +2,10 @@
 
 #Step4: Now start make drivers
 
-openwrt_release_source_file=openwrt-v22.03.0-rc1.tar.gz
-openwrt_release_source_dir=openwrt-v22.03.0-rc1
-openwrt_release_sdk_file=openwrt-sdk-22.03.0-rc1-ramips-rt305x_gcc-11.2.0_musl.Linux-x86_64.tar.xz
-openwrt_release_sdk_dir=openwrt-sdk-22.03.0-rc1-ramips-rt305x_gcc-11.2.0_musl.Linux-x86_64
+openwrt_release_source_file=openwrt-v22.03.0-rc4.tar.gz
+openwrt_release_source_dir=openwrt-v22.03.0-rc4
+openwrt_release_sdk_file=openwrt-sdk-22.03.0-rc4-ramips-rt305x_gcc-11.2.0_musl.Linux-x86_64.tar.xz
+openwrt_release_sdk_dir=openwrt-sdk-22.03.0-rc4-ramips-rt305x_gcc-11.2.0_musl.Linux-x86_64
 backport_source_file=backports-5.15.33-1.tar.xz
 backport_source_dir=backports-5.15.33-1
 
