35c938eb3d36fa23297233cb3bcd85112d48934e
chalesyu <574249312@qq.com>
2023-02-11 17:30:40 +0800

    bump version to v22.03.3
    not test

--- step2_decompress_files.sh
+++ step2_decompress_files.sh
@@ -5,12 +5,12 @@ echo
 echo Do decompress files...
 echo
 
-openwrt_release_source_file=openwrt-v22.03.0.tar.gz
-openwrt_release_source_dir=openwrt-v22.03.0
-openwrt_release_sdk_file=openwrt-sdk-22.03.0-ramips-rt305x_gcc-11.2.0_musl.Linux-x86_64.tar.xz
-openwrt_release_sdk_dir=openwrt-sdk-22.03.0-ramips-rt305x_gcc-11.2.0_musl.Linux-x86_64
-backport_source_file=backports-5.15.58-1.tar.xz
-backport_source_dir=backports-5.15.58-1
+openwrt_release_source_file=openwrt-v22.03.3.tar.gz
+openwrt_release_source_dir=openwrt-v22.03.3
+openwrt_release_sdk_file=openwrt-sdk-22.03.3-ramips-rt305x_gcc-11.2.0_musl.Linux-x86_64.tar.xz
+openwrt_release_sdk_dir=openwrt-sdk-22.03.3-ramips-rt305x_gcc-11.2.0_musl.Linux-x86_64
+backport_source_file=backports-5.15.81-1.tar.xz
+backport_source_dir=backports-5.15.81-1
 
 echo OpenWRT release source code is ${openwrt_release_source_file}
 echo OpenWRT SDK is ${openwrt_release_sdk_file}
--- step4_start_make_drivers.sh
+++ step4_start_make_drivers.sh
@@ -2,12 +2,12 @@
 
 #Step4: Now start make drivers
 
-openwrt_release_source_file=openwrt-v22.03.0.tar.gz
-openwrt_release_source_dir=openwrt-v22.03.0
-openwrt_release_sdk_file=openwrt-sdk-22.03.0-ramips-rt305x_gcc-11.2.0_musl.Linux-x86_64.tar.xz
-openwrt_release_sdk_dir=openwrt-sdk-22.03.0-ramips-rt305x_gcc-11.2.0_musl.Linux-x86_64
-backport_source_file=backports-5.15.58-1.tar.xz
-backport_source_dir=backports-5.15.58-1
+openwrt_release_source_file=openwrt-v22.03.3.tar.gz
+openwrt_release_source_dir=openwrt-v22.03.3
+openwrt_release_sdk_file=openwrt-sdk-22.03.3-ramips-rt305x_gcc-11.2.0_musl.Linux-x86_64.tar.xz
+openwrt_release_sdk_dir=openwrt-sdk-22.03.3-ramips-rt305x_gcc-11.2.0_musl.Linux-x86_64
+backport_source_file=backports-5.15.81-1.tar.xz
+backport_source_dir=backports-5.15.81-1
 
 cd ${openwrt_release_sdk_dir}/
 make V=s defconfig
